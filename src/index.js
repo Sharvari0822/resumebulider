f991589d478a4389a46800fa4320d074baa6e883
justvarad123 <33244550+justvarad123@users.noreply.github.com>
2023-05-30 13:44:28 +0530

    made changes

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import { Provider } from "react-redux";
-// import { store } from "./Redux/store";
-import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <Provider store={store}>
     <App />
-  </Router>
+  </Provider>
 );
-reportWebVitals();
