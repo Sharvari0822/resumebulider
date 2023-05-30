82957702d5f370762548e932674e09ee5cbd6d72
justvarad123 <33244550+justvarad123@users.noreply.github.com>
2023-05-30 14:08:33 +0530

    Updates

--- src/Component/BlackScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../Styles/BlackScreen.css";
-
-const BlackScreen = (props) => {
-  return <div className="black-screen"></div>;
-};
-
-export default BlackScreen;
